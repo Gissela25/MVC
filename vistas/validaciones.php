3edbf4c134ffe233a4adbafa204e0782c3a736c3
Lord-Jony <62217008+Jony-07@users.noreply.github.com>
2022-02-09 16:35:31 -0600

    Validación-Login
    Para poder continuar necesito la base de datos

---
+++ vistas/validaciones.php
@@ -0,0 +1,27 @@
+<?php
+    function isVoid($variable)
+    {
+        return empty(trim($variable));
+    }
+    function isAlphanumeric($variable)
+    {
+        return preg_match('/^[a-zA-Z0-9()._,*%&#@]{8,}$/',$variable);
+    }
+    function isText($variable)
+    {
+        return preg_match('/^[a-zA-Záéíóúñ ]{3,65}$/',$variable);
+    }
+    function isMail($variable)
+    {
+        return filter_var($variable,FILTER_VALIDATE_EMAIL);
+    }
+    function isUser($variable)
+    {
+        return preg_match('/^[a-zA-Z0-9-.]{4,20}$/',$variable);
+    }
+    function isEqual($variable1,$variable2)
+    {
+        return ($variable1==$variable2)?true:false;
+    }
+
+?>
