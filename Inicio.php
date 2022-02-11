c927de3547fb28c075dcc1fe628422758d04ea56
Gissela25 <gisselaverenice@gmail.com>
2022-02-11 11:49:22 -0600

    Inicio

---
+++ Inicio.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+    <link rel="stylesheet" href="assets/css/inicio.css">
+</head>
+
+<body>
+    <div class="header">
+        <div class="menu-bar">
+        <nav class="navbar navbar-expand-lg navbar navbar-dark" style="background-color: #000000;">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"></a>
+            <img src="assets/images/logo5.jpeg" alt="" width="120" height="50" class="d-inline-block align-text-top">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Inciar Sesion</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="?c=producto">Registrarse</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+        </div>
+    </div>
+</body>
+
+</html>
