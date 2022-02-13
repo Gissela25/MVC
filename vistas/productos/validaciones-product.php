405d7abbfc2c4bd44541afee124ceb7e4dd9d4ee
Lord-Jony <62217008+Jony-07@users.noreply.github.com>
2022-02-13 11:49:25 -0600

    Expresiones regulares

--- vistas/productos/validaciones-product.php
+++ vistas/productos/validaciones-product.php
@@ -16,4 +16,9 @@
         return preg_match('/^([0-9])?$/',$variable);
     }
 
+    //Pattern para producto y marca /^(([a-zA-Záéíóúñ]{3,65})[ ]?([a-zA-Záéíóúñ]{3,65})?)+$/
+    //Pattern para precio /^([0-9]+).?([0-9]+)?$/
+    //Pattern para numerocantidad  /^[0-9]+$/  
+    // En los inputs se agrega así pattern="/^[0-9]+$/"; Dependiendo de lo que pide
+    
 ?>
